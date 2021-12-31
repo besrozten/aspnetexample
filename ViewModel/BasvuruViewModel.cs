4bc311b567d7eb40d8fa8d5a5b52d5623a341128
BEŞİR ÖZTEN <92337931+besrozten@users.noreply.github.com>
2021-12-31 16:25:31 +0300

    Add files via upload

---
+++ ViewModel/BasvuruViewModel.cs
@@ -0,0 +1,14 @@
+﻿using stajdeneme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace stajdeneme.ViewModel
+{
+    public class BasvuruViewModel
+    {
+        public List<tbl_basvuru> Tbl_Basvurus { get; set; } 
+        public List<tbl_yetkinlik> Tbl_Yetkinliks { get; set; } 
+    }
+}
